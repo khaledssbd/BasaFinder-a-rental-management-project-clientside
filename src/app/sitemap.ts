017d6ee7c23ca00081416673fbb938e58a67d9c5
khaledssbd <khaledssbd@gmail.com>
2025-04-26 02:45:09 +0600

    priority & changeFrequency is updated in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,52 +11,70 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: 'https://basafinder-clientside.vercel.app',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+      changeFrequency: 'daily', // homepage is changes often
+      priority: 1.0,
+    },
+    {
+      url: 'https://basafinder-clientside.vercel.app/add-rental',
+      lastModified: new Date(),
+      changeFrequency: 'yearly', // add-rental route: rarely changes
+      priority: 0.3,
+    },
+    {
+      url: 'https://basafinder-clientside.vercel.app/register',
+      lastModified: new Date(),
+      changeFrequency: 'yearly', // authentication route: rarely changes
+      priority: 0.3,
+    },
+    {
+      url: 'https://basafinder-clientside.vercel.app/login',
+      lastModified: new Date(),
+      changeFrequency: 'yearly', // login route: rarely changes
+      priority: 0.3,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/about-us',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+      changeFrequency: 'yearly',
+      priority: 0.4,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/faq',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.7,
+      priority: 0.5,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/terms-and-conditions',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
+      changeFrequency: 'yearly',
+      priority: 0.4,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/privacy-policy',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
+      changeFrequency: 'yearly',
+      priority: 0.4,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/news',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.6,
+      changeFrequency: 'yearly',
+      priority: 0.4,
     },
     {
       url: 'https://basafinder-clientside.vercel.app/rentals',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.6,
+      changeFrequency: 'daily', // new rentals are added daily
+      priority: 0.9,
     },
 
     // rental details dynamically
     ...rentals.map((rental: IRental) => ({
       url: `https://basafinder-clientside.vercel.app/rentals/${rental._id}`,
       lastModified: rental.updatedAt,
-      changeFrequency: 'daily',
-      priority: 0.6,
+      changeFrequency: 'weekly', // rental info updates weekly
+      priority: 0.8,
     })),
   ];
 
